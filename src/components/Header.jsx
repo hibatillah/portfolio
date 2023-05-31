e530243e4586f4cc16b1abb3e617b3bdece900bf
hibatillah <hibatillah21si@mahasiswa.pcr.ac.id>
2023-05-31 23:54:52 +0700

    add nav, add label bookmarks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,97 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { profile, menu, darkToggle, grainy } from "../assets";
+import { NavLink } from "react-router-dom";
+
+const Navigasi = ({ handle }) => {
+  const menu = [
+    { name: "Home", path: "/" },
+    { name: "Projects", path: "/projects" },
+    { name: "Bookmarks", path: "/bookmarks" },
+  ];
+
+  return (
+    <ul className="w-full font-outfit">
+      {menu.map((item, index) => (
+        <NavLink
+          to={item.path}
+          key={index}
+          className={({ isActive }) =>
+            isActive ? "text-primary text-medium" : "text-grey-dark"
+          }
+        >
+          <li onClick={handle} className="text-lg/loose flex justify-center items-center gap-2 group/menu">
+            {item.name}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-4 h-4 transition-all group-hover/menu:translate-x-1"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+              />
+            </svg>
+          </li>
+        </NavLink>
+      ))}
+    </ul>
+  );
+};
 
 const Header = () => {
+  const [openMenu, setOpenMenu] = useState(false)
+  const handleMenu = () => setOpenMenu(!openMenu)
+
+  // darkmode toggle
+  const activeTheme = document.documentElement.getAttribute("class");
+  const [theme, setTheme] = useState("light");
+  const handleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+  useEffect(() => {
+    console.log("🚀 ~ file: Menu.jsx:8 ~ Menu ~ activeTheme:", activeTheme);
+    document.documentElement.setAttribute("class", theme);
+  }, [theme, activeTheme]);
+  
   return (
     <>
-      <header className="flex items-center justify-between px-4 py-3 select-none">
-        <a href="/">
-          <img
-            src={profile}
-            alt="logo"
-            className="w-8 h-8 rounded-lg pointer-events-none"
-          />
-        </a>
-        <div className="flex items-center gap-6">
-          <img
-            src={darkToggle}
-            alt="darkmode-toggle"
-            className="w-6 h-6 press"
-          />
-          <img src={menu} alt="menu-toggle" className="w-7 h-7 press" />
-        </div>
-      </header>
+      <div className="relative">
+        <header className="absolute w-full bg-white flex items-center justify-between px-4 py-3 select-none z-10">
+          <a href="/">
+            <img
+              src={profile}
+              alt="logo"
+              className="w-8 h-8 rounded-lg pointer-events-none"
+            />
+          </a>
+          <div className="flex items-center gap-6">
+            <img
+              src={darkToggle}
+              alt="darkmode-toggle"
+              className="w-6 h-6 press"
+              onClick={handleTheme}
+            />
+            <img
+              src={menu}
+              alt="menu-toggle"
+              className="w-7 h-7 press"
+              onClick={handleMenu}
+            />
+          </div>
+        </header>
+        <nav
+          className={`${
+            openMenu ? "top-14 " : "-top-40"
+          } absolute left-0 z-0 w-full px-4 py-6 flex justify-center bg-white border-y border-grey-light transition-all ease-out duration-300`}
+        >
+          <Navigasi handle={handleMenu} />
+        </nav>
+      </div>
       <img
         src={grainy}
         alt="grainy-background"
--- src/pages/Bookmarks.jsx
+++ src/pages/Bookmarks.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+
+const Label = () => {
+  const labels = [
+    "inspirations",
+    "icons",
+    "illustrations",
+    "fonts",
+    "colors",
+    "photos",
+    "mockups",
+    "patterns",
+  ];
+
+  const [activeLabel, setActiveLabel] = useState("inspirations");
+  const handleActive = (el) => setActiveLabel(el);
+  useEffect(() => {
+    console.log("🚀 ~ file: Bookmarks.jsx:6 ~ activeLabel", activeLabel);
+  },[activeLabel])
+
+  return (
+    <div className="flex flex-wrap gap-3">
+      {labels.map((item, index) => (
+        <div
+          key={index}
+          onClick={() => handleActive(item)}
+          className={`${
+            item === activeLabel
+              ? "bg-primary text-white border-primary"
+              : "text-primary border-grey-light bg-white"
+          }
+            w-fit px-5 py-2 border rounded-xl cursor-pointer select-none`}
+        >
+          {item}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 const Bookmarks = () => {
   return (
-    <div>Bookmarks</div>
-  )
-}
+    <main className="container font-outfit">
+      <div>
+        <h1>Bookmarks</h1>
+        <p className="mt-3">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+          nam repudiandae esse incidunt velit blanditiis consectetur deleniti
+          aspernatur officiis non!
+        </p>
+        <div className="mt-5">
+          <Label />
+        </div>
+      </div>
+      <div className="mt-12 mb-10 grid grid-cols-1 gap-6"></div>
+    </main>
+  );
+};
 
-export default Bookmarks
+export default Bookmarks;
